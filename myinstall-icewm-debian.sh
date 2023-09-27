da323c6ba4f014316d940ed0f8564f73bbaceab9
BlackStar <66219401+giuliano29@users.noreply.github.com>
2023-09-27 01:14:09 -0300

    Add files via upload

---
+++ myinstall-icewm-debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#==============#
+#MY DEBIAN STABLE INSTALL
+#=========================
+
+#INSTALL PROGRAMS
+sudo apt install xorg network-manager-gnome network-manager-openvpn-gnome\
+		powerline keepassxc gimp geany geany-plugins sakura pavucontrol\
+		atril galculator htop git powerline flameshot chromium chromium-l10n\
+		paper-icon-theme moka-icon-theme papirus-icon-theme pavucontrol locales-all\
+		pcmanfm mpv gpicview lightdm xdg-user-dirs xdg-user-dirs-gtk
+
+# INSTALL ICEWM
+sudo apt install icewm lxappearance picom volumeicon-alsa
+ 
