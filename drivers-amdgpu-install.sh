bd00b57d88977dc958dba7cba59c242be00eac2a
BlackStar <66219401+giuliano29@users.noreply.github.com>
2023-09-27 01:27:31 -0300

    Update drivers-amdgpu-install.sh

--- drivers-amdgpu-install.sh
+++ drivers-amdgpu-install.sh
@@ -5,3 +5,4 @@
 sudo dpkg --add-architecture i386 && sudo apt update
 sudo apt-get install firmware-amd-graphics libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all
 sudo apt install libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+sudo apt install lm-sensors fancontrol
